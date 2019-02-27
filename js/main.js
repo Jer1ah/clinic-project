3a9653552eb039e65db849eb1a1d4b7cdf0baca4
Jeriah Bowers <jeriah97bowers@yahoo.com>
2019-02-26 23:28:25 -0500

    Updating flexbox

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const mobileNav = document.getElementsByClassName("mobile-nav")[0];
+const navList = document.getElementsByClassName("nav__list")[0];
+
+///Adding mobile nav on click functionality
+mobileNav.addEventListener("click", () => {
+    if( navList.style.display != "flex" ) {
+        navList.style.display = "flex";
+    } else if( navList.style.display == "flex" ) {
+        navList.style.display = "none";
+    }
+});
+
+//Adding functionality so that the menu doesn't disappear on larger screens
+$(window).resize(() => {
+    if( $(window).innerWidth() > 830 ) {
+        navList.style.display = "flex";
+    } else {
+        navList.style.display = "none";
+    }
+});
+
+//Adding menu/close button functionality on click
+mobileNav.addEventListener("click", () => {
+    if( mobileNav.src.slice(22) == "img/close.svg" ) {
+        mobileNav.src = "img/menu.svg";
+    } else {
+        mobileNav.src = "img/close.svg";
+    }
+});
